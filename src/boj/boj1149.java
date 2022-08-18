6074f44dc7668446ab4c784c71706461b1c58647
UNCLE-20210624W\owner <sshj9027@naver.com>
2022-08-18 17:52:08 +0900

    Dynamic Programming

---
+++ src/boj/boj1149.java
@@ -0,0 +1,18 @@
+package boj;
+
+import java.util.Scanner;
+
+public class boj1149 {
+    public static void main(String[] args) {
+        /**
+         * Dynamic Programming
+         *
+         * 1번 집의 색은 2번 집의 색과 같지 않아야 한다.
+         * N번 집의 색은 N-1번 집의 색과 같지 않아야 한다.
+         * i(2 ≤ i ≤ N-1)번 집의 색은 i-1번, i+1번 집의 색과 같지 않아야 한다.
+         *
+         * 2 ≤ N ≤ 1,000
+         */
+        Scanner sc = new Scanner(System.in);
+    }
+}
---
+++ src/boj/boj11726.java
@@ -0,0 +1,26 @@
+package boj;
+
+import java.util.Scanner;
+
+public class boj11726 {
+    public static void main(String[] args) {
+        /**
+         * Dynamic Programming
+         *
+         * 2×n 크기의 직사각형을 1×2, 2×1 타일로 채우는 방법의 수를 구하는 프로그램을 작성하시오.
+         * 1 ≤ n ≤ 1,000
+         */
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[] dp = new int[1001];
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 3;
+        for (int i = 4; i <= n; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+        }
+        System.out.println(dp[n]);
+    }
+}
