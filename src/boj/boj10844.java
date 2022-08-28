12bd6224bdc648a4d650a3949571e1a4240f8df6
UNCLE-20210624W\owner <sshj9027@naver.com>
2022-08-28 22:14:07 +0900

    dynamic programming

---
+++ src/boj/boj10844.java
@@ -0,0 +1,24 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class boj10844 {
+    public static void main(String[] args) throws IOException {
+        /**
+         * Dynamic Programming
+         *
+         *
+         */
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int arr[] = new int[100];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt()
+        }
+
+    }
+}
